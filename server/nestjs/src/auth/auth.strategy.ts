9a2ff90a10b959ea44645bf764c4926ab0245241
John Henderson <john.henderson@energyweb.org>
2021-01-21 15:05:50 -0800

    [MYEN-545] AcceptedRoles from ENV in server apps

--- server/nestjs/src/auth/auth.strategy.ts
+++ server/nestjs/src/auth/auth.strategy.ts
@@ -5,11 +5,13 @@ import { PassportStrategy } from '@nestjs/passport';
 @Injectable()
 export class AuthStrategy extends PassportStrategy(LoginStrategy, 'login') {
   constructor() {
+    const acceptedRoles = process.env.ACCEPTED_ROLES ? process.env.ACCEPTED_ROLES.split(',') : [];
     super({
       jwtSecret: 'secret',
       name: 'login',
       rpcUrl: 'https://volta-rpc.energyweb.org/',
       cacheServerUrl: 'https://volta-iam-cacheserver.energyweb.org/',
+      acceptedRoles
     });
   }
 }
