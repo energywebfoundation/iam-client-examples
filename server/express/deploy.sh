0552598290c840335d52e74f2276e651ef3d028b
John Henderson <john.henderson@energyweb.org>
2021-02-05 09:47:43 -0800

    remove sudo from deploy.sh
    If user is in docker group, then can run without sudo

--- server/express/deploy.sh
+++ server/express/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 git pull
-sudo docker build . -t passport-did-auth-express:latest
-sudo docker-compose -f docker-compose.prod.yml up -d
+docker build . -t passport-did-auth-express:latest
+docker-compose -f docker-compose.prod.yml up -d
