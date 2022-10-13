ab92977771eb954718209e84eef0a478f4a755c3
dawidgil <dawid.gil@j-labs.pl>
2022-10-13 10:25:23 +0200

    feat: add logo and adjust button styles

--- client/angular-dapp/src/app/login/login.component.ts
+++ client/angular-dapp/src/app/login/login.component.ts
@@ -11,7 +11,7 @@ import { Observable } from 'rxjs';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
   providers = ProviderType;
