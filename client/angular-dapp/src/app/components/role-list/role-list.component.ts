157e061364d96b3ee6738a211c60d041dc0ef679
dawidgil <dawid.gil@j-labs.pl>
2022-10-13 11:42:25 +0200

    feat: beautify role list table

--- client/angular-dapp/src/app/components/role-list/role-list.component.ts
+++ client/angular-dapp/src/app/components/role-list/role-list.component.ts
@@ -4,7 +4,7 @@ import { Role } from '../../models/role';
 @Component({
   selector: 'app-role-list',
   templateUrl: './role-list.component.html',
-  styleUrls: ['./role-list.component.css']
+  styleUrls: ['./role-list.component.scss']
 })
 export class RoleListComponent implements OnInit {
   @Input() roles: Role[];
